37db09fd8ec3082f467c2360fa1521a1941ac738
AyeshaN816 <ayeshanareen563@gmail.com>
2024-10-01 16:20:45 +0530

    first time code

---
+++ Demo.java
@@ -0,0 +1,22 @@
+public class Demo{
+
+       public static void main(String[] args){
+
+              short noOfDaysInAYear=365;            
+              byte num=43;
+              int studentsRegForScolarchip=676354645;
+              int BooksInALibrary=547457425;
+              int PeopleInAStadium=365754598;
+
+              byte age=21;
+              int numOfVillages=56763;
+	   
+	      System.out.println(noOfDaysInAYear);	
+	      System.out.println(num);
+	      System.out.println(studentsRegForScolarchip);
+	      System.out.println(BooksInALibrary);
+              System.out.println(PeopleInAStadium);
+    	      System.out.println(age);
+    	      System.out.println(numOfVillages);
+       }
+}
