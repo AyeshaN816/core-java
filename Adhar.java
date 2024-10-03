4ade46147c9089e49c5c33d4a9cc86d555374eb0
AyeshaN816 <ayeshanareen563@gmail.com>
2024-10-03 11:01:20 +0530

    second push

---
+++ Adhar.java
@@ -0,0 +1,14 @@
+public class Adhar{
+
+       public static void main(String[] args){
+            num(34654323);
+            num(81477767);
+
+       }
+
+public static void num(int adharnum){
+       System.out.println("Your adharnum is:" + adharnum);
+       }
+}
+
+
